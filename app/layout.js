e18281fe174f1de20701f167dd47a2a55694b716
Gabriel Fernandes <60583301+gabrielcoffee@users.noreply.github.com>
2024-10-08 18:41:31 -0400

    Uploading all files
    only node_modules is excluded from the upload

---
+++ app/layout.js
@@ -0,0 +1,59 @@
+import localFont from "next/font/local";
+import { Fugaz_One, Open_Sans } from 'next/font/google'
+import "./globals.css";
+
+const opensans = Open_Sans({
+  subsets: ['latin'],
+})
+
+const fugaz = Fugaz_One({
+  subsets: ['latin'],
+  weight: ['400'],
+})
+
+/*
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+*/
+
+
+
+export const metadata = {
+  title: "Broodl",
+  description: "Track your daily mood everyday of the year!",
+};
+
+export default function RootLayout({ children }) {
+  const header = (
+    <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
+      <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>Broodl</h1>
+      <div className="flex items-center justify-between">
+        PLACEHOLDER CTA STATS
+      </div>
+    </header>
+  )
+
+  const footer = (
+    <footer className="p-4 sm:p-8 grid place-items-center">
+      <p className={'text-indigo-700 ' + fugaz.className}>Created with &lt;3</p> 
+    </footer>
+  )
+
+  return (
+    <html lang="en">
+      <body className={' w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800 ' + opensans.className}>
+        {header}
+        {children}
+        {footer}
+      </body>
+    </html>
+  );
+}
---
+++ components/Button.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Fugaz_One } from 'next/font/google';
+
+const fugaz = Fugaz_One({
+  subsets: ['latin'],
+  weight: ['400'],
+})
+
+export default function Button(props) {
+
+  const { text, dark, full } = props;
+
+  return (
+    <button className={' border border-solid border-indigo-600 duration-200 overflow-hidden rounded-full hover:opacity-60 ' + 
+    (dark ?
+      ' text-white bg-indigo-600 border-indigo-600 ' 
+    : 
+      ' text-indigo-600 ') +
+    (full ?
+      'grid place-items-center w-full' : ' '
+    )}>
+      <p className={"px-4 sm:px-10 whitespace-nowrap py-2 sm:py-3 " + fugaz.className}>
+      {text}
+      </p>
+    </button>
+  )
+}
